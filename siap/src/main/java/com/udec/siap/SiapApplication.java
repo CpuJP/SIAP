9ce620c5dfe52989224619955b1f77e1ce75c0ac
CpuJP <cpujuanpis@gmail.com>
2022-09-12 08:01:15 -0500

    Prueba push

---
+++ siap/src/main/java/com/udec/siap/SiapApplication.java
@@ -0,0 +1,13 @@
+package com.udec.siap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiapApplication.class, args);
+	}
+
+}
---
+++ siap/src/test/java/com/udec/siap/SiapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udec.siap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
