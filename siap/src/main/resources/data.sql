e5f6fccd987153b7d444d1e459fa994c52128826
CpuJP <cpujuanpis@gmail.com>
2022-10-23 21:22:12 -0500

    se actualiza el despliegue

---
+++ siap/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO role (description, name) VALUES ('Administrador', 'ADMIN');
+INSERT INTO role (description, name) VALUES ('Usuario', 'USER');
+INSERT INTO role (description, name) VALUES ('Bienestar', 'BIENS');
+INSERT INTO role (description, name) VALUES ('Biblioteca', 'BIBLIO');
+INSERT INTO role (description, name) VALUES ('Sala Computo', 'SALC');
+INSERT INTO role (description, name) VALUES ('Laboratorio', 'LABT');
+INSERT INTO role (description, name) VALUES ('Ingreso Campus', 'CAMP');
