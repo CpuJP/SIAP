a1d5148040f1fa84fbc762208ae7aff24de3c56e
CpuJP <cpujuanpis@gmail.com>
2022-10-31 08:28:44 -0500

    configuracion cors

--- src/main/java/com/project/SIAP/security/config/CORSFilter.java
+++ src/main/java/com/project/SIAP/security/config/CORSFilter.java
@@ -14,6 +14,7 @@ public class CORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization, Origin, Accept, Access-Control-Request-Method, Access-Control-Request-Headers");
+        response.setHeader("*", "*");
         chain.doFilter(req, res);
     }
 
